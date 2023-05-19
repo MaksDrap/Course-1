91f2491caccf4d71bcd812dcbc3a68b4c6323452
Сіплий <maksdrap228@gmail.com>
2023-05-19 18:02:05 +0300

    Добавлення таблиці де показано Input i Output

--- Project.py
+++ Project.py
@@ -23,17 +23,19 @@ def inverse_pBlock(input_data, permutation_table_inverse):
     return output_data
 
 def test_s_block():
-    # Test 1
+    # Test case 1
     input_data = [0, 1, 2, 3]
     s_table = [1, 3, 0, 2]
     expected_output = [1, 3, 0, 2]
 
+    # Check forward transformation
     s_block_output = s_block(input_data, s_table)
     print("Forward transformation of S-block:")
     print("Input       :", input_data)
     print("Output      :", s_block_output)
     print("Expected    :", expected_output)
 
+    # Check inverse transformation
     s_table_inverse = [2, 0, 3, 1]
     inverse_s_block_output = inverse_Sblock(s_block_output, s_table_inverse)
     print("Inverse transformation of S-block:")
@@ -43,28 +45,25 @@ def test_s_block():
     assert s_block_output == expected_output
     assert set(inverse_s_block_output) == set(input_data)
 
-    # Test  2
+    # Test case 2
+    s_table_inverse = [2, 0, 1, 3, 7, 4, 5, 6]
     input_data = [0, 1, 2, 3, 4, 5, 6, 7]
     s_table = [3, 0, 1, 2, 7, 4, 5, 6]
     expected_output = [3, 0, 1, 2, 7, 4, 5, 6]
 
-    s_block_output = s_block(input_data, s_table)
-    print("Forward transformation of S-block:")
-    print("Input       :", input_data)
-    print("Output      :", s_block_output)
-    print("Expected    :", expected_output)
-
-    s_table_inverse = [1, 3, 2, 0, 7, 4, 5, 6]
-    inverse_s_block_output = inverse_Sblock(s_block_output, s_table_inverse)
-    print("Inverse transformation of S-block:")
-    print("Input       :", s_block_output)
-    print("Output      :", inverse_s_block_output)
-    print("Expected    :", input_data)
-    assert s_block_output == expected_output
-    assert set(inverse_s_block_output) == set(input_data)
+    print("S-block transformation:")
+    print("-----------------------")
+    print("Input       | Output")
+    print("-----------------------")
+    for i in range(0, len(input_data), 2):
+        input_value = input_data[i:i + 2]
+        s_block_output = s_block(input_value, s_table)
+        inverse_s_block_output = inverse_Sblock(s_block_output, s_table_inverse)
+        print(f"{input_value}   | {s_block_output}")
+        print(f"             | {inverse_s_block_output}")
+    print("-----------------------")
 
     print("S-block test passed successfully.")
-
 
 def test_p_block():
     # Test 1
